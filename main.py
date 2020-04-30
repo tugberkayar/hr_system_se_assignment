f5c782e01f929a5d5da08b1aefee80a2cdc4ef5b
Etkin0 <49615906+Etkin0@users.noreply.github.com>
2020-05-01 02:15:36 +0300

    if name main try except
    yine bir şeyler denedim

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-#from src.Employee import Employee
-#from src.Project import Project
-#from src.Accounting import Accounting
 from src.ProjectManagement import ProjectManagement
+from src.DataHandler import DataHandler
 
-#a = em.Analyst(1000, 'tugberk', 1, accounting=ac1)
-#d = em.Designer(400, 'ipek', 2, accounting=ac2)
-#m = em.Manager(20000, 'dogan', 3,accounting= ac1)
-#p = em.Programmer(2000, 'etkin', 4,accounting= ac1)
-#t = em.Tester(3, 'deniz', 5, accounting=ac1)
 
-#create project manager
-ProjectManager = ProjectManagement()
-#create an example project
-ProjectManager.create_project(0,2,3)
+def main():
+    # create project manager
+    ProjectManager = ProjectManagement()
+    
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print("\033[1;31m", end="")
+        print(e)
+        print("\033[0;0m")
+    else:
+        print("\033[1;32mEverything was fine\033[0;0m")
+    finally:
+        print("\033[1;33mShutdown\033[0;0m")
