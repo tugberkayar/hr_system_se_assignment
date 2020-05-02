ddceab4f0ab34f48a3bbcfe9a48b0f55ce60f455
Etkin0 <49615906+Etkin0@users.noreply.github.com>
2020-05-02 23:32:20 +0300

    reach from project management

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 import pandas as pd
+import numpy as np
 from kivy.uix.screenmanager import Screen
 from src.DataHandler import DataHandler
 from src.Employee import Employee
@@ -13,24 +14,21 @@ class HomeScreen(Screen):
 
         self.__data_handler = DataHandler()
         self.__project_manager = ProjectManagement(
-            self.__data_handler.load_current_employees(),
-            self.__data_handler.load_applicants()
+            self.__data_handler.load_current_projects(),
+            self.__data_handler.load_current_employees()
         )
-
-        self.__employees_dict = self.__data_handler.load_current_employees()
         self.__applicant_list = self.__data_handler.load_applicants()
-        self.__projects_dict = self.__data_handler.load_current_projects()
         self.__retrieve_project_employees()
-        self.ids.employees.values = ["Name:{name}, ID:{id}".format(name=e.name, id=e.id) for e in self.__employees_dict.values()]
+        self.ids.employees.values = ["Name:{name}, ID:{id}".format(name=e.name, id=e.id) for e in self.__project_manager.employees.values()]
         self.ids.applicants.values = ["Name:{name}, ID:{id}".format(name =a['name'], id=a['id'] ) for a in self.__applicant_list]
-        self.ids.projects.values = [str(p.id) for p in self.__projects_dict.values()]
+        self.ids.projects.values = [str(p.id) for p in self.__project_manager.projects.values()]
 
     def hire_button_on_click(self):
         if not self.ids.applicants.text == "Choose an applicant":
             selected_id = int(self.ids.applicants.text.split(":")[-1])
             applicant = self.__applicant_list[selected_id]
             random_salary = 4000 + np.random.rand(1)[0] * 1000
-            employee = Employee(salary=random_salary, name=applicant['name'], emp_id=)
+            #employee = Employee(salary=random_salary, name=applicant['name'], emp_id=)
 
     def new_project_on_click(self):
         pass
@@ -48,18 +46,18 @@ class HomeScreen(Screen):
         pass
 
     def __retrieve_project_employees(self):
-        for e in self.__employees_dict.values():
+        for e in self.__project_manager.employees.values():
             if not pd.isna(e.project_id):
-                self.__projects_dict[e.project_id].employees[e.id] = e
+                self.__project_manager.projects[e.project_id].employees[e.id] = e
 
     def employee_spinner_select(self):
         selected_id = int(self.ids.employees.text.split(":")[-1])
-        employee = self.__employees_dict[selected_id]
+        employee = self.__project_manager.employees[selected_id]
         self.ids.employee_info.text = str(employee)
     
     def project_spinner_select(self):
         selected_id = int(self.ids.projects.text)
-        project = self.__projects_dict[selected_id]
+        project = self.__project_manager.projects[selected_id]
         self.ids.project_info.text = str(project)
 
     def applicant_spinner_select(self):
